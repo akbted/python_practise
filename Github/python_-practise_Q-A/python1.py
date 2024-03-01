dd274211b52796d27bd9a132db426369742d2288
akbted <anantha.b@btech.christuniversity.in>
2024-03-01 10:00:01 +0530

    third commit

--- Github/python_-practise_Q-A/python1.py
+++ Github/python_-practise_Q-A/python1.py
@@ -3,3 +3,6 @@ print("hello world!") # to print a string
 print("Hi,", "\nI am learning Python!") #To print a string in two lines
 print(input("What is your name? ")) #Taking input and printing a string
 print("Hello" + " How are you ?") #Concat two strings 
+
+#Integers 
+print(1+2) #This will print the sum 

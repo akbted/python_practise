2de328c4faea8c0f2641912e8c928cead50e136c
akbted <anantha.b@btech.christuniversity.in>
2024-03-01 09:52:47 +0530

    Second commit

--- Github/python_-practise_Q-A/python1.py
+++ Github/python_-practise_Q-A/python1.py
@@ -1,4 +1,5 @@
 #Strings
 print("hello world!") # to print a string 
 print("Hi,", "\nI am learning Python!") #To print a string in two lines
-print(input("What is your name? "))
+print(input("What is your name? ")) #Taking input and printing a string
+print("Hello" + " How are you ?") #Concat two strings 

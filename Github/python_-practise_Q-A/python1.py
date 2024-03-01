a82e728f4e8705cc3e6a1312f63281f047e8ae3d
Anantha Krishna B <157831286+akbted@users.noreply.github.com>
2024-03-01 10:22:24 +0530

    Delete Github directory

--- Github/python_-practise_Q-A/python1.py
+++
@@ -1,9 +0,0 @@
-#Strings
-print("hello world!") # to print a string 
-print("Hi,", "\nI am learning Python!") #To print a string in two lines
-print(input("What is your name? ")) #Taking input and printing a string
-print("Hello" + " How are you ?") #Concat two strings 
-
-#Integers 
-print(1+2) #This will print the sum 
-print("Hi!" * 5) #This will print hi 5 times
